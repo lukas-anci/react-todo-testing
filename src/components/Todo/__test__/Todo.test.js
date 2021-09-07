9a5304c93c1deecde9c44e2b976efc6422ea6d70
Lukas <lukas.ancikevicius@gmail.com>
2021-09-07 08:28:42 +0300

    multiple add test, helper fn, nottohave class test

--- src/components/Todo/__test__/Todo.test.js
+++ src/components/Todo/__test__/Todo.test.js
@@ -32,10 +32,38 @@ it('Should add single todo to a list ', () => {
 
 function addToTodo(todoArr) {
   // ivedam reiksme, paspaudziam mygtuka cikle
+  const inputEl = screen.getByPlaceholderText(/Add a new task/);
+  // gauti mygtuka
+  const btnEl = screen.getByRole('button', { name: /add/i });
+  // ivedam reiksme
+
+  todoArr.forEach((todo) => {
+    fireEvent.change(inputEl, { target: { value: todo } });
+    // paspaudziam mygtuka
+    fireEvent.click(btnEl);
+  });
 }
 
 it('Shoud add multiple todos to a list', () => {
   // taip pat kaip auksciau tik ivedam ir paspaudziam 3 kartus
   // vietoj rankinio ivedimo, pasidaryti funkcija kuri gauna masyva
   // ir iveda masyvo el reiksme ir paspaudzia mygtuka
+
+  render(<MockTodo />);
+  addToTodo(['one', 'two', 'three']);
+  const todoAddedArr = screen.getAllByTestId('todo-item');
+  // Assert
+  expect(todoAddedArr).toHaveLength(3);
+  //   expect(todoAddedEl).toHaveClass('todo-item');
 });
+
+it('Task should not have completed class when added to list', () => {
+  render(<MockTodo />);
+  addToTodo(['Do sports on Sunday']);
+  const todoAddedEl = screen.getByText('Do sports on Sunday');
+  // Assert
+
+  expect(todoAddedEl).not.toHaveClass('todo-item-active');
+});
+
+test('Task should be gray when clicked (add a class)', () => {});
