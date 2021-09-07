589e613bc678a9363aa38ba8047bc21e592b5d4b
Lukas <lukas.ancikevicius@gmail.com>
2021-09-07 11:02:55 +0300

    mock server using MSW

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -1,4 +1,7 @@
-import { screen, render } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import FollowersList from './../FollowersList';
 
@@ -9,16 +12,42 @@ const MockFlist = () => {
     </BrowserRouter>
   );
 };
+const mockResponse = {
+  results: [
+    {
+      name: {
+        first: 'James',
+        last: 'Bond',
+      },
+      login: {
+        username: 'zerozerozeven',
+      },
+      picture: {
+        large: 'https://www.google.lt',
+      },
+    },
+  ],
+};
+
+const server = setupServer(
+  rest.get('https://randomuser.me/api/', (req, res, ctx) => {
+    return res(ctx.json(mockResponse));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('Async testing', () => {
-  it('Renders Followers on the screen', async () => {
-    render(<MockFlist />);
-    // screen.debug();
-    const contactElArr = await screen.findAllByTestId(/contact-el-/);
-    expect(contactElArr).not.toHaveLength(0);
-    expect(contactElArr).toHaveLength(1);
-    // expect(contactElArr).toBeInTheDocument();
-  });
+  // it('Renders Followers on the screen', async () => {
+  //   render(<MockFlist />);
+  //   // screen.debug();
+  //   const contactElArr = await screen.findAllByTestId(/contact-el-/);
+  //   expect(contactElArr).not.toHaveLength(0);
+  //   expect(contactElArr).toHaveLength(2);
+  //   // expect(contactElArr).toBeInTheDocument();
+  // });
 
   it('Renders one follower card on the screen', async () => {
     render(<MockFlist />);
