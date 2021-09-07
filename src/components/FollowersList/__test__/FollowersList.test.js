0a15d0ca4e900b9c203325e32e35691d17d71fff
Lukas <lukas.ancikevicius@gmail.com>
2021-09-07 08:57:50 +0300

    testing async followers list

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,20 @@
+import { screen, render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import FollowersList from './../FollowersList';
+
+const MockFlist = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+
+describe('Async testing', () => {
+  it('Renders Follower on the screen', async () => {
+    render(<MockFlist />);
+    // screen.debug();
+    const contactElArr = await screen.findAllByTestId('contact-el');
+    expect(contactElArr).not.toHaveLength();
+  });
+});
